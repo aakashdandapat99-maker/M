141b295cbab14b3201a2b41ed670b5ad31e04a34
aakashdandapat99-maker <aakashdandapat99@gmail.com>
2025-11-19 01:57:40 +0530

    Update __main__.py

--- VILLAIN_MUSIC/__main__.py
+++ VILLAIN_MUSIC/__main__.py
@@ -41,11 +41,11 @@ async def init():
     await userbot.start()
     await VILLAIN.start()
     await VILLAIN.decorators()
-    LOGGER("VILLAIN_MUSIC").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗩𝗜𝗟𝗟𝗔𝗜𝗡♨️\n╚═════ஜ۩۞۩ஜ════╝")
+    LOGGER("VILLAIN_MUSIC").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𓆩☠𓆪‌ٖٖٖٖٖٖٜٖٖٖٖ𝘼4𝙭🥀𝐃𝐀𝐑𝐊─⃛͢𓆪🇴𝐏𓆩☠𓆪‌ٖٖٖٖٖٖٜٖٖٖٖ♨️\n╚═════ஜ۩۞۩ஜ════╝")
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("VILLAIN_MUSIC").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗩𝗜𝗟𝗟𝗔𝗜𝗡♨️\n╚═════ஜ۩۞۩ஜ════╝")
+    LOGGER("VILLAIN_MUSIC").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𓆩☠𓆪‌ٖٖٖٖٖٖٜٖٖٖٖ𝘼4𝙭🥀𝐃𝐀𝐑𝐊─⃛͢𓆪🇴𝐏𓆩☠𓆪‌ٖٖٖٖٖٖٜٖٖٖٖ♨️\n╚═════ஜ۩۞۩ஜ════╝")
 
 
 if __name__ == "__main__":
